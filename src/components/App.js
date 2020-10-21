3e282df2f5e992f34a7c6c01e0efd4b7645006dc
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-21 21:46:19 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, setState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+    this.state = { sig: false };
+  }
+  handleClick = () => {
+    setState({
+      sig: true
+    });
+  };
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button id="click" onClick={() => this.handleClick()}>
+          {this.tate.sig ? (
+            <p id="para">
+              Hello, I've learnt to use the full-stack evaluation tool. This
+              makes me so happy
+            </p>
+          ) : null}
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-

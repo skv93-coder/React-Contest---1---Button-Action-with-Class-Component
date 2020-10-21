886fb459b1f953e69dae2bd04cb9cc52f31ca865
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-21 21:57:08 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,16 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = { sig: false };
   }
-  handleClick = () => {
-    setState({
-      sig: true
-    });
-  };
+
   render() {
+    const [sig, setSig] = setState(false);
+
     return (
       <div id="main">
         {/* Do not remove this main div!! */}
-        <button id="click" onClick={() => this.handleClick()}>
-          {this.tate.sig ? (
+        <button id="click" onClick={() => setState(true)}>
+          {sig ? (
             <p id="para">
               Hello, I've learnt to use the full-stack evaluation tool. This
               makes me so happy

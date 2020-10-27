02c92375cec4bd30df489b70ab9ab20c4ff6e433
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-27 16:44:17 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
-import React, { Component, setState } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = { para: "" };
   }
-
   render() {
-    const [sig, setSig] = setState(false);
-
     return (
       <div id="main">
-        {/* Do not remove this main div!! */}
-        <button id="click" onClick={() => setState(true)}>
-          {sig ? (
-            <p id="para">
-              Hello, I've learnt to use the full-stack evaluation tool. This
-              makes me so happy
-            </p>
-          ) : null}
+        <button
+          id="click"
+          onClick={() =>
+            this.setState({
+              para:
+                "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+            })
+          }
+        >
+          Button
         </button>
+        {this.state.para ? <p id="para">{this.state.para}</p> : ""}
       </div>
     );
   }
